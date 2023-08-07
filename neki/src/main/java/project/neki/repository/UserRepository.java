02f9d11bfc058f4506b52532cf5bb95b28004054
AllanFerreiraGomes <allanferreiragomes1@gmail.com>
2023-08-07 18:35:41 -0300

    security

---
+++ neki/src/main/java/project/neki/repository/UserRepository.java
@@ -0,0 +1,18 @@
+package project.neki.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import project.neki.model.User;
+
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer> {
+	Optional<User> findByUsername(String username);
+
+	Boolean existsByUsername(String username);
+
+	Boolean existsByEmail(String email);
+}
